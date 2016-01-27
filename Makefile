48262f1d0768336f557cc103a924548f51a98d53
lastavengers <lastavengers@outlook.com>
2016-01-28 00:24:06 +0800

    gtk3: gtk test

--- Makefile
+++ Makefile
@@ -7,24 +7,29 @@ MAKE = make -r
 CC = gcc
 DEL = rm -rf
 LD = ld
-CFLAGS = -Wall -Werror -Isrc/inc -m32 -ggdb -gstabs+
+CFLAGS = -Wall -Werror -Isrc/inc -ggdb -gstabs+
+GTK3FLAGS = `pkg-config --cflags gtk+-3.0`
+GTK3LIBS = `pkg-config --libs gtk+-3.0`
 
 build/srain: src/srain.c
-	$(CC) $(CFLAGS) $^ -o $@  
+	$(CC) $(CFLAGS) $(GTK3FLAGS) $(GTK3LIBS) $^ -o $@
+
+po: src/srain.c
+	xgettext --from-code=UTF-8 -o i18n/srain.pot -k_ -s src/srain.c
+	cd i18n/ && msginit --no-translator -i srain.pot
+
+mo:
+	msgfmt i18n/zh_CN.po -o build/locale/zh_CN/LC_MESSAGES/srain.mo
 
 init:
 	mkdir -p build > /dev/null
 	mkdir -p build/locale/zh_CN/LC_MESSAGES > /dev/null
 
-po: src/srain.c
-	xgettext --from-code=UTF-8 -o i18n/srain.pot -k_ -s src/srain.c
-	cd i18n && msginit --no-translator -i srain.pot
-
-mo:
-	msgfmt i18n/zh_CN.po -o build/locale/zh_CN/LC_MESSAGES/srain.mo
-
 default: Makefile
 	$(MAKE) bulid/srain
+
+run: build/srain
+	cd build/ && ./srain
 
 clean: 
 	$(DEL) build/*
--- src/srain.c
+++ src/srain.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
+#include <gtk/gtk.h>
 #include "i18n.h"
 
-int main(){
+static void print_hello (GtkWidget *widget, gpointer data){
+    g_print(_("Hello srain\n"));
+}
+
+static void activate(GtkApplication* app, gpointer user_data){
+    GtkWidget *window;
+    GtkWidget *button;
+    GtkWidget *button_box;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW (window), "Srain");
+    gtk_window_set_default_size(GTK_WINDOW (window), 200, 200);
+
+    button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+    gtk_container_add(GTK_CONTAINER(window), button_box);
+    button = gtk_button_new_with_label(_("Hello srain"));
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+    gtk_container_add(GTK_CONTAINER(button_box), button);
+
+    gtk_widget_show_all(window);
+}
+
+
+int main(int argc, char **argv){
+    int status;
+    GtkApplication *app;
+
     setlocale(LC_ALL, "");
     textdomain("srain");
     bindtextdomain("srain", "locale");
     printf(_("Hello srain!\n"));
-    return 0;
+
+
+    app = gtk_application_new("org.gtk.srain", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run (G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }
