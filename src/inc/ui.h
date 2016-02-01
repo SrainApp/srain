5a536f60e3bee9bafce6fda4107afcd89990722b
lastavengers <lastavengers@outlook.com>
2016-02-02 00:07:27 +0800

    ui: add popmenu of msg_bubble and add func ui_rm_chat and etc. * src/inc/ui.h: change structure of `Chan` and rename it to `Chat` * src/ui.c:     - add popmenu of all msg_bubble     - rename chan -> chat     - ui_rm_chat work now * ui/chat_panel.glade: add topic label, remove unuesdd widget * ui/msg_bubble.glade: add menu item * TODO: GtkMenu will be free after unref builder

--- src/inc/ui.h
+++ src/inc/ui.h
@@ -6,12 +6,12 @@
 
 typedef struct {
     char name[20];
-    GtkWidget *panel;   /* Must be a GtkBox */
-} Chan;
+    char *topic;
+} Chat;
 
 void ui_window_init();
-void ui_join_chan(const char *chan);
-void ui_part_chan(Chan chan);
+void ui_new_chat(const Chat chat);
+void ui_rm_chat(const char *chat_name);
 void ui_send_msg(const MsgSend msg);
 void ui_recv_msg(const MsgRecv msg);
 void ui_sys_msg(const MsgSys msg);
--- src/ui.c
+++ src/ui.c
@@ -12,8 +12,20 @@
 static GtkWidget *window;
 static GtkWidget *chat_panel_stack;
 
-static unsigned int nchan = 0;
-static Chan chan_list[100]; // TODO replace with MACRO
+static unsigned int nchat = 0;
+static struct {
+    Chat chat;
+    GtkWidget *msg_listbox;
+} chat_list[100]; // TODO replace with MACRO
+
+static gint msg_label_popup_handler(GtkWidget *label, GdkEvent *event, GtkWidget *menu){
+    if (event->button.button == 3 && !gtk_label_get_selection_bounds(GTK_LABEL(label), NULL, NULL)){
+        gtk_menu_popup(GTK_MENU(menu), NULL, NULL, NULL, NULL, event->button.button, event->button.time);
+        return TRUE;
+    }
+    return FALSE;
+}
+
 
 static void ui_apply_css(GtkWidget *widget, GtkStyleProvider *provider){
     gtk_style_context_add_provider(gtk_widget_get_style_context(widget), provider, G_MAXUINT);
@@ -26,12 +38,11 @@ void ui_window_init(){
     GtkBuilder *builder;
     GtkStyleProvider *provider;
 
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "../ui/window.glade", NULL);
+    builder = gtk_builder_new_from_file( "../ui/window.glade");
     UI_BUILDER_GET_WIDGET(builder, window);
     UI_BUILDER_GET_WIDGET(builder, chat_panel_stack);
 
-    // gtk_builder_connect_signals(builder, NULL);
+    gtk_builder_connect_signals(builder, NULL);
     g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
     /* load style */
@@ -49,29 +60,37 @@ void ui_window_init(){
     g_object_unref(G_OBJECT(provider));
 }
 
-void ui_join_chan(const char *chan){
+void ui_new_chat(const Chat chat){
     GtkBuilder *builder;
+    GtkWidget *chat_name_label;
+    GtkWidget *chat_topic_label;
     GtkWidget *chat_panel_box;
     GtkWidget *chat_msg_listbox;
 
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "../ui/chat_panel.glade", NULL);
+    builder = gtk_builder_new_from_file( "../ui/chat_panel.glade");
     UI_BUILDER_GET_WIDGET(builder, chat_panel_box);
     UI_BUILDER_GET_WIDGET(builder, chat_msg_listbox);
+    UI_BUILDER_GET_WIDGET(builder, chat_name_label);
+    UI_BUILDER_GET_WIDGET(builder, chat_topic_label);
 
-    gtk_stack_add_named(GTK_STACK(chat_panel_stack), chat_panel_box, (gchar *)chan);
-    gtk_container_child_set(GTK_CONTAINER(chat_panel_stack), chat_panel_box, "title", (gchar *)chan, NULL);
+    gtk_label_set_text(GTK_LABEL(chat_name_label), chat.name);
+    gtk_label_set_text(GTK_LABEL(chat_topic_label), chat.topic);
+    gtk_stack_add_named(GTK_STACK(chat_panel_stack), chat_panel_box, (gchar *)chat.name);
+    gtk_container_child_set(GTK_CONTAINER(chat_panel_stack), chat_panel_box, "title", (gchar *)chat.name, NULL);
 
-    strncpy(chan_list[nchan].name, chan, 20);
-    chan_list[nchan].panel = chat_msg_listbox;
-    printf("%s: %d\n", chan, nchan);
-    nchan++;
+    strncpy(chat_list[nchat].chat.name, chat.name, 20);
+    // chat_list[nchat].msg_listbox = g_object_ref(chat_msg_listbox);
+    chat_list[nchat].msg_listbox = chat_msg_listbox;
+    nchat++;
+    // TODO nchat will reach limit soon
 
     g_object_unref(G_OBJECT(builder));
 }
 
-void ui_part_chan(Chan chan){
-
+void ui_rm_chat(const char *chat_name){
+    GtkWidget *chat_panel_box = gtk_stack_get_child_by_name(GTK_STACK(chat_panel_stack), chat_name);
+    assert(chat_panel_box);
+    gtk_container_remove(GTK_CONTAINER(chat_panel_stack), chat_panel_box);
 }
 
 void ui_send_msg(const MsgSend msg){
@@ -81,21 +100,25 @@ void ui_send_msg(const MsgSend msg){
     GtkWidget *send_msg_label;
     GtkWidget *send_image;
     GtkWidget *send_time_label;
+    GtkWidget *msg_bubble_menu;
 
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "../ui/msg_bubble.glade", NULL);
+    builder = gtk_builder_new_from_file( "../ui/msg_bubble.glade");
     if (msg.img) UI_BUILDER_GET_WIDGET(builder, send_image);
     UI_BUILDER_GET_WIDGET(builder, send_msg_bubble_box);
     UI_BUILDER_GET_WIDGET(builder, send_msg_label);
     UI_BUILDER_GET_WIDGET(builder, send_time_label);
+    UI_BUILDER_GET_WIDGET(builder, msg_bubble_menu);
 
     gtk_label_set_text(GTK_LABEL(send_msg_label), msg.msg);
     gtk_label_set_text(GTK_LABEL(send_time_label), msg.time);
 
-    for (i = 0; i < nchan; i++){
-        if (strcmp(msg.chan, chan_list[i].name) == 0){
-            gtk_container_add(GTK_CONTAINER(chan_list[i].panel), GTK_WIDGET(send_msg_bubble_box));
-            return;
+    g_signal_connect(G_OBJECT(send_msg_label), "event", G_CALLBACK(msg_label_popup_handler), G_OBJECT(msg_bubble_menu));
+    g_object_ref(msg_bubble_menu); // TODO with out this statement, gtkmenu will be free after unref builder
+
+    for (i = 0; i < nchat; i++){
+        if (strcmp(msg.chan, chat_list[i].chat.name) == 0){
+            gtk_container_add(GTK_CONTAINER(chat_list[i].msg_listbox), GTK_WIDGET(send_msg_bubble_box));
+            break;
         }
     }
     g_object_unref(G_OBJECT(builder));
@@ -112,9 +135,9 @@ void ui_recv_msg(const MsgRecv msg){
     GtkWidget *identify_label;
     GtkWidget *recv_msg_label;
     GtkWidget *recv_time_label;
+    GtkWidget *msg_bubble_menu;
 
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "../ui/msg_bubble.glade", NULL);
+    builder = gtk_builder_new_from_file( "../ui/msg_bubble.glade");
     if (msg.avatar) UI_BUILDER_GET_WIDGET(builder, avatar_image);
     if (msg.img) UI_BUILDER_GET_WIDGET(builder, recv_image);
     UI_BUILDER_GET_WIDGET(builder, recv_msg_bubble_box);
@@ -123,17 +146,20 @@ void ui_recv_msg(const MsgRecv msg){
     UI_BUILDER_GET_WIDGET(builder, identify_label);
     UI_BUILDER_GET_WIDGET(builder, recv_msg_label);
     UI_BUILDER_GET_WIDGET(builder, recv_time_label);
+    UI_BUILDER_GET_WIDGET(builder, msg_bubble_menu);
 
     gtk_label_set_text(GTK_LABEL(nick_label), msg.nick);
     gtk_label_set_text(GTK_LABEL(identify_label), msg.id);
     gtk_label_set_text(GTK_LABEL(recv_time_label), msg.time);
     gtk_label_set_text(GTK_LABEL(recv_msg_label), msg.msg);
 
-    for (i = 0; i < nchan; i++){
-        printf("%s and %s %d\n", msg.chan, chan_list[i].name, i);
-        if (strcmp(msg.chan, chan_list[i].name) == 0){
-            gtk_container_add(GTK_CONTAINER(chan_list[i].panel), GTK_WIDGET(recv_msg_bubble_box));
-            return;
+    g_signal_connect(G_OBJECT(recv_msg_label), "event", G_CALLBACK(msg_label_popup_handler), G_OBJECT(msg_bubble_menu));
+    g_object_ref(msg_bubble_menu); // TODO with out this statement, gtkmenu will be free after unref builder
+
+    for (i = 0; i < nchat; i++){
+        if (strcmp(msg.chan, chat_list[i].chat.name) == 0){
+            gtk_container_add(GTK_CONTAINER(chat_list[i].msg_listbox), GTK_WIDGET(recv_msg_bubble_box));
+            break;
         }
     }
     g_object_unref(G_OBJECT(builder));
@@ -144,16 +170,15 @@ void ui_sys_msg(const MsgSys msg){
     GtkBuilder *builder;
     GtkWidget *sys_msg_label;
 
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "../ui/msg_bubble.glade", NULL);
+    builder = gtk_builder_new_from_file( "../ui/msg_bubble.glade");
     UI_BUILDER_GET_WIDGET(builder, sys_msg_label);
 
     gtk_label_set_text(GTK_LABEL(sys_msg_label), msg.msg);
 
-    for (i = 0; i < nchan; i++){
-        if (strcmp(msg.chan, chan_list[i].name) == 0){
-            gtk_container_add(GTK_CONTAINER(chan_list[i].panel), GTK_WIDGET(sys_msg_label));
-            return;
+    for (i = 0; i < nchat; i++){
+        if (strcmp(msg.chan, chat_list[i].chat.name) == 0){
+            gtk_container_add(GTK_CONTAINER(chat_list[i].msg_listbox), GTK_WIDGET(sys_msg_label));
+            break;
         }
     }
     g_object_unref(G_OBJECT(builder));
