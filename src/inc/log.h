593f6b9461daa27799250d6371f3b55512a1b1bd
lastavengers <lastavengers@outlook.com>
2016-02-04 16:12:46 +0800

    misc: modify log macro

--- src/inc/log.h
+++ src/inc/log.h
@@ -1,8 +1,20 @@
 #ifndef __LOG_H
 #define __LOG_H
 
-#define LOG(...) \
-    g_print("%d: [%s]: ", __LINE__, __FUNCTION__);   \
-    g_print(__VA_ARGS__)
+// log macro with Line number, Fucntion name and Return
+#define LOG_LFR(...)                                \
+    g_print("%d: [%s]: ", __LINE__, __FUNCTION__);  \
+    g_print(__VA_ARGS__);                           \
+    g_print("\n")
+
+// log macro with Fucntion name and Return
+#define LOG_FR(...)                     \
+    g_print("[%s]: ",  __FUNCTION__);   \
+    g_print(__VA_ARGS__);               \
+    g_print("\n")
+
+// log macro, equal to g_print
+#define LOG(...)            \
+    g_print(__VA_ARGS__);   \
 
 #endif /* __LOG_H */
