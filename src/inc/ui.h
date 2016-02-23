ec2296cba1a63b3399b9f0dbfe6e44aa3298488f
lastavengers <lastavengers@outlook.com>
2016-02-24 01:31:23 +0800

    remove unused code

--- src/inc/ui.h
+++ src/inc/ui.h
@@ -34,24 +34,4 @@ typedef struct {
     char msg[MSG_LEN];
 } sys_msg_t;
 
-void ui_window_init();
-void ui_msg_init();
-const char* ui_chan_get_cur();
-
-/* normal UI funcition, called in main loop; */
-int ui_msg_send(const char *chan, const char *msg, const char *img);
-int ui_chan_add(const char *chan);
-int ui_chan_rm(const char *chan);
-
-/* These function oftenly called as a idle, so thire type muse be:
- *      gboolean (*GSourceFunc) (gpointer user_data);
- * they accept a sturcture pointer which point to the parameters they required
- * this structure should be free at the end of function
- */
-gboolean ui_chan_set_topic(topic_t *topic_t);
-gboolean ui_online_list_add(online_list_item_t *item);
-gboolean ui_online_list_rm(online_list_item_t *item);
-gboolean ui_msg_recv(recv_msg_t *msg);
-gboolean ui_msg_sys(sys_msg_t *msg);
-
 #endif /* __UI_H */
