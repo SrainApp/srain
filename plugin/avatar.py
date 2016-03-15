d9ec89e510dd579d3ca7ff338e19b9855de508f3
lastavengers <lastavengers@outlook.com>
2016-03-15 14:46:18 +0800

    new plugin: avatar

---
+++ plugin/avatar.py
@@ -0,0 +1,10 @@
+##
+# @file avatar.py
+# @brief avatar plugin example
+# @author LastAvengers <lastavengers@outlook.com>
+# @version 1.0
+# @date 2016-03-15
+
+def avatar(nick, user, host):
+    print(nick, user, host)
+    return '/home/la/git/srain/build/avatar.png'
