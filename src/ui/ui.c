5eb6a4a41b1d81b1a3da21938d4b36f814704a6c
Shengyu Zhang <lastavengers@outlook.com>
2016-08-21 10:42:36 +0800

    Alter sys msg fallback

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -371,12 +371,10 @@ int ui_rm_chan_sync(const char *srv_name, const char *chan_name){
  * @brief Add a system message to sepcified channel
  *
  * @param srv_name
- * @param chan_name If no such channel, fallback to current channel.
- *                  If current channel doesn't belong to Server `srv_name`,
- *                  fallback to META_SERVER
+ * @param chan_name
  * @param msg
- * @param type If it is SYS_MSG_ACTION or SYS_MSG_ERROR,
- *          sidebar should be updated
+ * @param type SrainStackSidebar should be updated
+ *             when type = SYS_MSG_ACTION or SYS_MSG_ERROR or SYS_MSG_NOTICE
  */
 void ui_sys_msg_sync(const char *srv_name, const char *chan_name,
         const char *msg, SysMsgType type){
@@ -384,10 +382,20 @@ void ui_sys_msg_sync(const char *srv_name, const char *chan_name,
     SrainMsgList *list;
 
     chan = srain_window_get_chan_by_name(srain_win, srv_name, chan_name);
-    if (!chan)
-        chan = srain_window_get_cur_chan(srain_win);
-    if (strcmp(srain_chan_get_srv_name(chan), srv_name))
-        chan = srain_window_get_chan_by_name(srain_win, srv_name, META_SERVER);
+
+    if (!chan){
+        if (type == SYS_MSG_ERROR){
+            chan = srain_window_get_cur_chan(srain_win);
+
+            /* Current SrainChan doesn't belong to `srv_name` */
+            if (strcmp(srain_chan_get_srv_name(chan), srv_name)){
+                chan = srain_window_get_chan_by_name(srain_win, srv_name, META_SERVER);
+            }
+        } else {
+            chan = srain_window_get_chan_by_name(srain_win, srv_name, META_SERVER);
+        }
+    }
+
     g_return_if_fail(chan);
 
     list = srain_chan_get_msg_list(chan);
