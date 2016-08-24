c56e4ccadd63da52cef72aa323f0612fc63bfa58
Shengyu Zhang <lastavengers@outlook.com>
2016-08-25 02:36:15 +0800

    Fixed: conflict function definition

--- src/srv/srv_test.c
+++ src/srv/srv_test.c
@@ -9,60 +9,54 @@
 #include "log.h"
 #include "meta.h"
 
-void srv_test_ui_add_chan(const char *server_name, const char *chan_name){
-    LOG_FR("server_name: %s, chan_name: %s", server_name, chan_name);
+void srv_test_ui_add_chan(const char *srv_name, const char *chan_name){
+    LOG_FR("srv_name: %s, chan_name: %s", srv_name, chan_name);
 }
 
-void srv_test_ui_rm_chan(const char *server_name, const char *chan_name){
-    LOG_FR("server_name: %s, chan_name: %s", server_name, chan_name);
+void srv_test_ui_rm_chan(const char *srv_name, const char *chan_name){
+    LOG_FR("srv_name: %s, chan_name: %s", srv_name, chan_name);
 }
 
-void srv_test_ui_sys_msg(const char *server_name, const char *chan_name,
+void srv_test_ui_sys_msg(const char *srv_name, const char *chan_name,
         const char *msg, SysMsgType type){
-    LOG_FR("server_name: %s, chan_name: %s, msg: '%s', type: %d",
-            server_name, chan_name, msg, type);
+    LOG_FR("srv_name: %s, chan_name: %s, msg: '%s', type: %d",
+            srv_name, chan_name, msg, type);
 }
 
-void srv_test_ui_send_msg(const char *server_name, const char *chan_name,
+void srv_test_ui_send_msg(const char *srv_name, const char *chan_name,
         const char *msg){
-    LOG_FR("server_name: %s, chan_name: %s, msg: '%s'",
-            server_name, chan_name, msg);
+    LOG_FR("srv_name: %s, chan_name: %s, msg: '%s'",
+            srv_name, chan_name, msg);
 }
 
-void srv_test_ui_recv_msg(const char *server_name, const char *chan_name,
+void srv_test_ui_recv_msg(const char *srv_name, const char *chan_name,
         const char *nick, const char *id, const char *msg){
-    LOG_FR("server_name: %s, chan_name: %s, nick: %s, id: %s, msg: '%s'",
-            server_name, chan_name, nick, id, msg);
+    LOG_FR("srv_name: %s, chan_name: %s, nick: %s, id: %s, msg: '%s'",
+            srv_name, chan_name, nick, id, msg);
 }
 
-void srv_test_ui_user_list_add(const char *server_name, const char *chan_name,
+void srv_test_ui_user_list_add(const char *srv_name, const char *chan_name,
         const char *nick, UserType type){
-    LOG_FR("server_name: %s, chan_name: %s, nick: '%s', type: %d",
-            server_name, chan_name, nick, type);
+    LOG_FR("srv_name: %s, chan_name: %s, nick: '%s', type: %d",
+            srv_name, chan_name, nick, type);
 }
 
-void srv_test_ui_user_list_rm(const char *server_name, const char *chan_name,
+void srv_test_ui_user_list_rm(const char *srv_name, const char *chan_name,
         const char *nick){
-    LOG_FR("server_name: %s, chan_name: %s, nick: '%s'",
-            server_name, chan_name, nick);
+    LOG_FR("srv_name: %s, chan_name: %s, nick: '%s'",
+            srv_name, chan_name, nick);
 }
 
-void srv_test_ui_user_list_rm_all(const char *server_name, const char *nick,
-        const char *reason){
-    LOG_FR("server_name: %s, nick: '%s', reason: %s",
-            server_name, nick, reason);
+void srv_test_ui_user_list_rename(const char *srv_name, const char *chan_name, 
+        const char *old_nick, const char *new_nick, UserType type){
+    LOG_FR("srv_name: %s, chan_name: %s, old_nick: %s, new_nick: %s, type: %d",
+            srv_name, chan_name, old_nick, new_nick, type);
 }
 
-void srv_test_ui_user_list_rename(const char *server_name, const char *old_nick,
-        const char *new_nick, UserType type, const char *msg){
-    LOG_FR("server_name: %s, old_nick: %s, new_nick: %s, type: %d, msg: %s",
-            server_name, old_nick, new_nick, type, msg);
-}
-
-void srv_test_ui_set_topic(const char *server_name, const char *chan_name,
+void srv_test_ui_set_topic(const char *srv_name, const char *chan_name,
         const char *topic){
-    LOG_FR("server_name: %s, chan_name: %s, topic: '%s'",
-            server_name, chan_name, topic);
+    LOG_FR("srv_name: %s, chan_name: %s, topic: '%s'",
+            srv_name, chan_name, topic);
 }
 
 void srv_test(){
