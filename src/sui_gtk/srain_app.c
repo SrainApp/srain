97d9e1ecb4536a82dc49222a0782a68427be6df2
Shengyu Zhang <i@srain.im>
2017-08-06 09:52:05 +0800

    Sui: application command line options support

--- src/sui_gtk/srain_app.c
+++ src/sui_gtk/srain_app.c
@@ -24,7 +24,6 @@
  * @date 2016-03-01
  */
 
-
 #include <gtk/gtk.h>
 
 #include "theme.h"
@@ -45,8 +44,29 @@ SrainApp *srain_app = NULL;
 /* Only one SrainWindow instance in one application */
 SrainWindow *srain_win = NULL;
 
-static void srain_app_activate(GtkApplication *app){
+static GOptionEntry option_entries[] = {
+    {
+        .long_name = "version",
+        .short_name = 'v',
+        .flags = 0,
+        .arg = G_OPTION_ARG_NONE,
+        .arg_data = NULL,
+        .description = N_("Show version information"),
+        .arg_description = NULL,
+    },
+    {
+        .long_name = G_OPTION_REMAINING,
+        .short_name = '\0',
+        .flags = 0,
+        .arg = G_OPTION_ARG_STRING_ARRAY,
+        .arg_data = NULL,
+        .description = N_("Open one or more IRC URLs"),
+        .arg_description = N_("[URL...]")
+    },
+    {NULL}
+};
 
+static void activate(GApplication *app){
     if (srain_win){
         gtk_window_present(GTK_WINDOW(srain_win));
     } else {
@@ -57,24 +77,61 @@ static void srain_app_activate(GtkApplication *app){
     }
 }
 
+static gint handle_local_options(GApplication *app, GVariantDict *options,
+        gpointer user_data){
+    if (g_variant_dict_lookup(options, "version", "b", NULL)){
+        g_print("%s %s%s\n", PACKAGE_NAME, PACKAGE_VERSION, PACKAGE_BUILD);
+        return 0; // Exit
+    }
+
+    return -1; // Return -1 to let the default option processing continue.
+}
+
+static gint command_line(GApplication *app,
+        GApplicationCommandLine *cmdline, gpointer user_data){
+    int len;
+    gchar **urls;
+    GVariantDict *options;
+
+    options = g_application_command_line_get_options_dict(cmdline);
+
+    if (g_variant_dict_lookup(options, G_OPTION_REMAINING, "^as", &urls)){
+        len =  g_strv_length(urls);
+        sui_event_hdr(NULL, SUI_EVENT_OPEN, (const char **)urls, len);
+        g_strfreev(urls);
+    }
+
+    g_application_activate(app);
+
+    return 0;
+}
+
 static void srain_app_init(SrainApp *self){
     if (srain_app) return;
 
     srain_app = self;
+
+    g_application_add_main_option_entries(G_APPLICATION(self), option_entries);
+
+    g_signal_connect(self, "activate", G_CALLBACK(activate), NULL);
+    g_signal_connect(self, "command-line", G_CALLBACK(command_line), NULL);
+    g_signal_connect(self, "handle-local-options", G_CALLBACK(handle_local_options), NULL);
+
     return;
 }
 
 static void srain_app_class_init(SrainAppClass *class){
-    G_APPLICATION_CLASS(class)->activate =
-        (void *)(GApplication *)srain_app_activate;
 }
 
 SrainApp* srain_app_new(void){
     return g_object_new(SRAIN_TYPE_APP,
-            "application-id", "org.gtk.srain", NULL);
+            "application-id", "org.srain.srain",
+            "flags", G_APPLICATION_HANDLES_COMMAND_LINE,
+            NULL);
 }
 
 void srain_app_quit(SrainApp *app){
+    /*
     GtkWidget *win;
     GList *list, *next;
 
@@ -87,4 +144,6 @@ void srain_app_quit(SrainApp *app){
 
         list = next;
     }
+    */
+    g_application_quit(G_APPLICATION(app));
 }
