1abeaacdc796dd94fcc86adefadfc0cd7e07bfcc
Shengyu Zhang <i@silverrainz.me>
2018-04-05 15:39:57 +0800

    Sirc(impl): sirc_target_is_xxx() utils functions

--- src/inc/sirc/sirc_utils.h
+++ src/inc/sirc/sirc_utils.h
@@ -25,16 +25,10 @@
 
 #include "srain.h"
 
-#define CHAN_PREFIX1    '#'
-#define CHAN_PREFIX2    '&'
-
-#define sirc_is_chan(ch) (ch && (ch[0] == CHAN_PREFIX1 || ch[0] == CHAN_PREFIX2))
-
-bool sirc_nick_cmp(const char *nick1, const char *nick2);
-bool sirc_prefix_is_server(const char *prefix);
-
-const char* sirc_prefix_get_nick(const char *prefix);
-const char* sirc_prefix_get_host(const char *prefix);
-const char* sirc_prefix_get_user(const char *prefix);
+bool sirc_target_equal(const char *t1, const char *t2);
+bool sirc_target_is_servername(SircSession *sirc, const char *target);
+bool sirc_target_is_nickname(SircSession *sirc, const char *target);
+bool sirc_target_is_service(SircSession *sirc, const char *target);
+bool sirc_target_is_channel(SircSession *sirc, const char *target);
 
 #endif /* __SIRC_UTILS_H */
