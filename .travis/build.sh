278fc7f5f294adec39a45e0abf10d2828bc678ea
Tom <hexuxin@foxmail.com>
2020-03-03 01:26:27 +0800

    Replace Travis CI with Github actions.

--- .travis/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /build
-./configure                     \
-    --prefix=/usr/local         \
-    --datadir=/usr/local/share  \
-    --sysconfdir=/etc
-make
--- .travis/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /build
-make install
