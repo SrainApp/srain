50e5cc96aeb487ba81dbdc1b2020fee4162232d0
Shengyu Zhang <i@silverrainz.me>
2022-07-30 00:19:31 +0800

    Script(update): bump version

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ baseurl = 'https://srain.silverrainz.me/'
 # built documents.
 #
 # The short X.Y version.
-version = '1.4.0'
+version = '1.4.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
